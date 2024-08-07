9d4ba14b45a2144e3852a9229697f15e065a22a7
evandrajr <evandra.jerolin@sma-yosuka.sch.id>
2024-08-07 14:40:23 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Kelompok 7";
+	cin.ignore();
+	cout << endl;
+	cout << "NIS" << endl;
+	cout << "1. 09\n"
+		"2. 23241058\n"
+		"3. 20211097\n" << endl;
+	cin.ignore();
+	cout << "Nama" << endl;
+	cout << "1. Evandra Jerolin\n"
+		"2. Jonathan Alexander\n"
+		"3. Renardo Nicholas Tan\n" << endl;
+	cin.ignore();
+	cout << "Kelas 11.2" << endl;
+	cin.ignore();
+	cout << "Jurusan IPA" << endl;
+	cin.ignore();
+	cout << "Jenis Kelamin" << endl;
+	cout << "1. Evandra Jerolin (l)\n"
+		"2. Jonathan Alexander (L)\n"
+		"3. Renardo Nicholas Tan (L)\n" << endl;
+	cin.ignore();
+	cout << "Alamat" << endl;
+	cout << "1. Evandra Jerolin - Jalan Mashudi No. 67, Cikampek\n"
+		"2. Jonathan Alexander - Jalan Cikangkung, Rengasdengklok\n"
+		"3. Renardo Nicholas Tan - Grand Taruma Blok N1/E/01\n" << endl;
+	system("PAUSE");
+	return 0;
+}
